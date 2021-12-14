b76a7f843f9d8c87f85876816150714dae35e87f
AlfredoSantiz210698 <alfredosantiz52@gmail.com>
2021-12-14 00:30:56 -0600

    Disabled rejectUnauthorized

--- config/database.js
+++ config/database.js
@@ -94,7 +94,7 @@ module.exports = {
       user: Env.get("DB_USER", DATABASE_URL.username),
       password: Env.get("DB_PASSWORD", DATABASE_URL.password),
       database: Env.get("DB_DATABASE", DATABASE_URL.pathname.substr(1)),
-      ssl: true
+      ssl: { rejectUnauthorized: false },
     },
     debug: Env.get("DB_DEBUG", false),
   },
