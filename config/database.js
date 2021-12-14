3d877febdb9f2e6e663341559495a051852d1099
AlfredoSantiz210698 <alfredosantiz52@gmail.com>
2021-12-14 00:27:34 -0600

    Enabled pg ssl

--- config/database.js
+++ config/database.js
@@ -94,7 +94,7 @@ module.exports = {
       user: Env.get("DB_USER", DATABASE_URL.username),
       password: Env.get("DB_PASSWORD", DATABASE_URL.password),
       database: Env.get("DB_DATABASE", DATABASE_URL.pathname.substr(1)),
-      // ssl: true
+      ssl: true
     },
     debug: Env.get("DB_DEBUG", false),
   },
